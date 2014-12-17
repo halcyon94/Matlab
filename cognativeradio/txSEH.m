6b8b8b9454aa0dfc03447a2d6ffeadede88cc562
halcyon94 <resuto@cooper.edu>
2014-12-16 20:01:26 -0500

    Update txSEH.m

--- cognativeradio/txSEH.m
+++ cognativeradio/txSEH.m
@@ -15,13 +15,13 @@ M = 16;   % THIS IS THE M-ARY # for the FSK MOD.  You have 16 channels available
 
 % initialize, will be set by rx after 1st transmission
 if isempty(feedbackSEH)
-    feedbackSEH = bi2de([0 0 0 0 0 0 0 1]);
+    feedbackSEH = bi2de([0 0 0 0 0 1 1 1]);
 end
 feedback_values = de2bi(feedbackSEH,8);
 %% You should edit the code starting here
 
-arr_p = [1 2 4];
-tonecoeff = 2*sum(arr_p.*feedback_values(6:8))
+arr_p = [2 4 8];
+tonecoeff = sum(arr_p.*feedback_values(6:8));
 
 Mod_Bit_values = {[4 1003],[8 828],[16 698],[16 808],[16 873],[16 923],[16 973],[16 993],[16 1003],[64 708],[64 788],...
                   [64 838],[64 893],[64 933],[64 963],[64 983],[64 983],[64 1003],[64 1013],[64 1013],[64 1013]};
@@ -29,10 +29,10 @@ chan_values = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
            
 ChanToMod = containers.Map(chan_values,Mod_Bit_values);
 arr_p = [1 2 4 8 16];
-chan_num = sum(arr_p.*feedback_values(1:5))
+chan_num = sum(arr_p.*feedback_values(1:5));
 tot = ChanToMod(chan_num);
-msgM = tot(1)
-BCH_k = tot(2)
+msgM = tot(1);
+BCH_k = tot(2);
 k = log2(msgM);
 
 BCH_n = 1023;
@@ -42,7 +42,7 @@ bits_enc_inter = bchenc(gf(bits_resh),BCH_n,BCH_k);
 bits_enc_final = [ones(1024*k-(floor(1024*k/BCH_n)*BCH_n),1);reshape(double(bits_enc_inter.x).',[],1);];
 
 syms = bi2de(reshape(bits_enc_final,k,length(bits_enc_final)/k).','left-msb')';
-msg = qammod(syms,msgM);
+msg = qammod(syms,msgM,0,'gray');
 msglength = length(msg);
 
 if(msglength ~= 1024)
